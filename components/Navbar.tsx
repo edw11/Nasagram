7cca9334629199e35dbb4f8dbb972dc1f39738fa
Edward <edw.chydi@gmail.com>
2025-01-11 20:10:47 +0900

    add scrollTop

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,6 +9,13 @@ const Navbar = () => {
   const { setTheme, resolvedTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Smooth scroll animation
+    });
+  };
+
   // Ensure the component only renders on the client
   useEffect(() => {
     setMounted(true);
@@ -20,7 +27,12 @@ const Navbar = () => {
     <div className="fixed w-full z-10">
       <div className="px-5 py-3 dark:bg-gray-800 bg-white dark:border-none border-b-2 border-solid border-gray-300 dark:text-white text-black flex justify-center">
         <div className="flex justify-between items-center w-[55rem]">
-          <p className="font-title text-3xl lg:text-4xl">Nasagram</p>
+          <p
+            className="font-title text-3xl lg:text-4xl cursor-pointer"
+            onClick={scrollToTop}
+          >
+            Nasagram
+          </p>
           <div className="flex gap-5 md:gap-6">
             {resolvedTheme === "dark" ? (
               <Sun
